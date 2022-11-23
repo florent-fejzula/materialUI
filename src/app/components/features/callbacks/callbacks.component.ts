1f309f443db9856ad6c7c6d6769256e8685571e8
Florent <dodi-messi19@hotmail.com>
2022-11-23 06:48:35 +0100

    callback and http post/get updates, good

--- src/app/components/features/callbacks/callbacks.component.ts
+++ src/app/components/features/callbacks/callbacks.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./callbacks.component.scss']
 })
 export class CallbacksComponent implements OnInit {
+  output = '';
+
+  posts = [
+    { title: 'Post One', body: 'This is body one' },
+    { title: 'Post Two', body: 'This is body two' }
+  ];
 
   constructor() { }
 
@@ -14,6 +20,27 @@ export class CallbacksComponent implements OnInit {
       this.firstAction(() => 
         this.secondAction('Im second'), 'Im first')
       , 3000);
+
+    // basic Promise example
+    const myPromise  = new Promise((res, rej) => {
+      let conn = true;
+
+      if(conn) {
+        res('Connection estabilished');
+      }
+      else {
+        rej('Connection refused');
+      }
+    });
+
+    myPromise.then((message) => {
+      console.log(message);
+    }).catch((message) => {
+      console.log(message);
+    });
+
+    // this.getPosts();
+    this.createPost({ title: 'Post Three', body: 'This is body three' }).then(this.getPosts);
   }
 
   firstAction(callback: any, message: string) {
@@ -23,6 +50,36 @@ export class CallbacksComponent implements OnInit {
 
   secondAction(message: string) {
     console.log(message);
+  }
+
+  getPosts() {
+    setTimeout(() => {
+      console.log('OUTPUT: ', this.output);
+      this.output = '';
+      console.log('OUTPUT 2: ', this.output);
+      this.posts.forEach((post, index) => {
+        this.output += post.title;
+      });
+    }, 1000);
+  }
+
+  createPost(post: { title: string; body: string; }) {
+    console.log('POSTSSS: ', this.posts);
+    
+    return new Promise<void>((resolve, reject) => {
+      setTimeout(() => {
+        this.posts.push(post);
+
+        const error = false;
+
+        if(!error){
+          resolve();
+        }
+        else {
+          reject('Error! Smth ewrnrt ower');
+        }
+      }, 2000);
+    });
   }
 
 }
--- src/app/components/features/http/http.component.ts
+++ src/app/components/features/http/http.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-http',
@@ -16,6 +17,7 @@ export class HttpComponent implements OnInit {
 
   ngOnInit(): void {
     this.getUsers();
+    this.fetchPosts();
   }
 
   getUsers() {
@@ -40,6 +42,32 @@ export class HttpComponent implements OnInit {
   getProduct() {
     this.http.get('http://something.com/api/product').subscribe((res) => {
       console.log(res);
+    });
+  }
+
+  // http POST with subscribe
+  onCreatePost(postData: { title: string; content: string }) {
+    this.http.post('https://dodi-web-default-rtdb.europe-west1.firebasedatabase.app/posts.json', postData).subscribe(responseData => {
+      console.log(responseData);
+    });
+  }
+
+  getPosts() {
+    console.log('fetching posts:');
+    this.fetchPosts();
+  }
+
+  // http GET with subscribe
+  fetchPosts() {
+    this.http.get('https://dodi-web-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+    .pipe(map((responseData: { [x: string]: any; }) => {
+      const postsArray = [];
+      for (const key in responseData) {
+        postsArray.push({ ...responseData[key], id: key });
+      }
+    }))
+    .subscribe(posts => {
+      console.log(posts);
     });
   }
 
